48c4d116f5a6528707c9bcb670cf507327c2c36a
Pål Hamel <pal.hamel@gmail.com>
2020-09-24 14:38:11 +0200

    import dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// firebase
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+// hooks
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
