f4caa3b3acaad468b71bbfd7536f982f0da8b9e5
Pål Hamel <pal.hamel@gmail.com>
2020-09-24 18:54:38 +0200

    add chatroom function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "firebase/firestore";
 import "firebase/auth";
 // hooks
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 // init firebase
 firebase.initializeApp({
@@ -22,16 +22,19 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
+
 // APP
 
-export function App() {
+function App() {
   // user auth
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
-      <header></header>
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+      <header><h2>-----HEADER-------</h2></header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
@@ -45,12 +48,53 @@ function SignIn() {
   return <button onClick={signInWithGoogle}>Sign In with Google</button>;
 }
 // sign out if current user exists:
-function SignOut() {
+
+// function SignOut() {
+//   return auth.currentUser && (
+//     <button onClick={() => auth.signOut()}>Sign Out</button>
+//   );
+// }
+
+function ChatRoom() {
+  // connect to a firebase db collection:
+  const messagesRef = firestore.collection('messages');
+  // query items in collection:
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  console.log('query sent:', query);
+  // check collection for new data:
+  const [messages] = useCollectionData(query, { idField: 'id' });
+  
+  console.log('messages in db:', messages);
+
+  // Verkar som att jag inte har kontakt med databasen?
+  
+
   return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
-  );
+    
+    <>
+    hey hey ChatRoom here
+  
+    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+    </>
+
+)
+
 }
 
-function ChatRoom() {}
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  return (
+    <>
+    
+      <div>
+        ChatMessage here
+        <p>{text}</p>
+      </div>
+    </>
+  )
+}
 
-// export default App;
+
+
+export default App;
