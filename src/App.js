02844d41e30a38ec6c2d25f05aa14eb1523c9dd7
Pål Hamel <pal.hamel@gmail.com>
2020-09-24 15:56:31 +0200

    add signin button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // firebase
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 // hooks
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollection } from 'react-firebase-hooks/firestore'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
+// init firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAPwGQIzXAcl2zE1U9lyEwWryVrXMDt3l4",
+  authDomain: "firechat-e7b2e.firebaseapp.com",
+  databaseURL: "https://firechat-e7b2e.firebaseio.com",
+  projectId: "firechat-e7b2e",
+  storageBucket: "firechat-e7b2e.appspot.com",
+  messagingSenderId: "602227103276",
+  appId: "1:602227103276:web:2241c9a60d14d69c8b2389",
+});
 
-
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  // user auth 
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header>
+
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign In with Google</button>
+  )
+}
+
+function ChatRoom() {
+
 }
 
 export default App;
