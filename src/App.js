e53bc16c755faa90528e262e4422ded0caff5c84
Pål Hamel <pal.hamel@gmail.com>
2020-09-26 20:20:15 +0200

    Add input field for chat message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState} from "react";
 import "./App.css";
 // firebase
 import firebase from "firebase/app";
@@ -32,6 +32,8 @@ function App() {
   return (
     <div className="App">
       <header><h2>-----HEADER-------</h2></header>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+
       <section>
         {user ? <ChatRoom /> : <SignIn />}
       </section>
@@ -49,49 +51,74 @@ function SignIn() {
 }
 // sign out if current user exists:
 
-// function SignOut() {
-//   return auth.currentUser && (
-//     <button onClick={() => auth.signOut()}>Sign Out</button>
-//   );
-// }
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
 
 function ChatRoom() {
+  // const dummy = useRef;
   // connect to a firebase db collection:
   const messagesRef = firestore.collection('messages');
   // query items in collection:
   const query = messagesRef.orderBy('createdAt').limit(25);
-  console.log('query sent:', query);
+  // console.log('query sent:', query);
   // check collection for new data:
   const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+  // event handler - listen to change - create new document in firestore:
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+    setFormValue('');
+  }
   
-  console.log('messages in db:', messages);
+  // console.log('messages in db:', messages);
 
-  // Verkar som att jag inte har kontakt med databasen?
-  
 
-  return (
-    
-    <>
-    hey hey ChatRoom here
-  
-    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
 
-    </>
+  return (<>
+    <main>
 
-)
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      {/* <span ref={dummy}></span> */}
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+
+      <button type="submit"> submit button</button>
+
+    </form>
+  </>)
 
 }
 
 function ChatMessage(props) {
-  const { text, uid } = props.message;
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
   return (
-    <>
-    
-      <div>
-        ChatMessage here
-        <p>{text}</p>
-      </div>
-    </>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="photoURL"/>
+      <p>{text}</p>
+    </div>
   )
 }
 
